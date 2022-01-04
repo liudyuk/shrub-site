2cf50078908a52d4cd92f36411fc56006f684b7a
N-Acil <47857258+N-Acil@users.noreply.github.com>
2022-01-04 13:12:31 +0000

    Add files via upload

---
+++ SoilContent_Shrub_sites.js
@@ -0,0 +1,128 @@
+//--------------------------------------------------------
+// Authors:
+// Script: Nezha Acil, University of Birmingham
+// Site data: Daijun Liu, University of Birmingham
+//--------------------------------------------------------
+
+Map.setZoom(0);
+
+// Parameters to edit
+var siteFile = "Daijun/shrub_site_NA";  // Name of the file that contains site id and their locations in latlon WGS84
+var id = 'ID'; // Name of the column containing site id 
+
+var radiusBuffer = 500; // Buffer width to apply in meters
+//var res = 250 // resolution of processing in meters (to align with SoilGrids)
+ 
+// Updated list of available layers in GEE SoilGrid https://git.wur.nl/isric/soilgrids/soilgrids.notebooks/-/blob/master/markdown/access_on_gee.md 
+//-------------------------------------------------------------------------------------------------------------------------------------------------
+// bdod_mean > Bulk density)
+// cec_mean > Cation exchange capacity at pH7
+// cfvo_mean > Coarse fragments
+// clay_mean > Clay
+// nitrogen_mean > Total Nitrogen
+// ocd_mean > Organic carbon density
+// ocs_mean > Organic carbon stock
+// phh2o_mean > pH in H2O
+// sand_mean > Sand
+// silt_mean > Silt
+// soc_mean > Soil organic carbon
+
+var lyr_name = 'bdod_mean'; // Layer to import from isric dataset https://git.wur.nl/isric/soilgrids/soilgrids.notebooks/-/blob/master/markdown/access_on_gee.md
+
+
+// Import
+var plots = ee.FeatureCollection("users/NXA807/"+siteFile);
+var lyr = ee.Image(ee.String("projects/soilgrids-isric/"+ lyr_name));
+
+
+// Mapping
+Map.addLayer(lyr,{}, lyr_name);
+Map.addLayer(plots);
+
+
+
+//--------------
+// Extract stats 
+//--------------
+
+// Add a band summarising all the layer's bands (depths 0-5cm, 5-15cm,15-30cm,30-60cm,60-100cm,100-200cm) (mean, median etc.) 
+var lyr_mn = lyr.reduce(ee.Reducer.mean());
+var lyr = lyr.addBands(lyr_mn.rename("Mean of all layers"));
+var bandNames = lyr.bandNames(); 
+
+
+// Points
+var addVar = function (f1) { 
+  var addVarBand = function (band, f2) {
+     band = ee.String(band)
+     f2 = ee.Feature(f2)
+    var varBand = ee.String(band)
+    var l = lyr.select(band);
+    var r = l.reduceRegion({
+            geometry: f1.geometry(),
+            reducer: ee.Reducer.first(),
+            //scale: res
+            }).get(band)
+                return f2.set(
+                  varBand, r
+                  )
+            } 
+  var f = ee.Feature(bandNames.iterate(addVarBand, f1))
+    return ee.Feature(null, f.toDictionary(
+      bandNames.add(id)))
+}
+
+var outPts = plots.map(addVar);
+print(outPts.first().toDictionary(), 'Data extracted for one example plot point');
+
+
+// Buffers around points
+var addVar = function (f1) { 
+  var addVarBand = function (band, f2) {
+     band = ee.String(band)
+     f2 = ee.Feature(f2)
+    var varBand = ee.String(band)
+    var l = lyr.select(band);
+    var r = l.reduceRegion({
+            geometry: f1.geometry().buffer(radiusBuffer),
+            reducer: ee.Reducer.mean(),
+            //scale: res
+            }).get(band)
+                return f2.set(
+                  varBand, r
+                  )
+            } 
+  var f = ee.Feature(bandNames.iterate(addVarBand, f1))
+    return ee.Feature(null, f.toDictionary(
+      bandNames.add(id)))
+};
+
+var outBuf = plots.map(addVar);
+print(outBuf.first().toDictionary(), 'Data extracted for one example buffered plot point');
+
+
+
+//----------------------
+// Export
+//----------------------
+
+var tabPts = ee.FeatureCollection(outPts.toList(outPts.size()));
+print(tabPts.limit(5));
+Export.table.toDrive({
+    collection: tabPts, 
+    folder: 'Shrub_sites_update_2021-12-31/',
+    description: 'Shrub_sites_NA_Soil_'+lyr_name+'_plotsPoints',
+    fileFormat: 'CSV'
+});
+
+var tabBuf = ee.FeatureCollection(outBuf.toList(outBuf.size()));
+Export.table.toDrive({
+    collection: tabBuf , 
+    folder: 'Shrub_sites_update_2021-12-31/',
+    description: 'Shrub_sites_NA_Soil_'+lyr_name+'_'+'plotsBuffered'+radiusBuffer+'m',
+    fileFormat: 'CSV'
+});
+
+
+
+
---
+++ SoilMoisture_Shrub_sites.js
@@ -0,0 +1,106 @@
+//--------------------------------------------------------
+// Authors:
+// Script: Nezha Acil, University of Birmingham
+// Site data: Daijun Liu, University of Birmingham
+//--------------------------------------------------------
+
+
+Map.setZoom(0);
+
+//--------------------
+// Parameters to edit
+//--------------------
+var siteFile = "Daijun/shrub_site_NA"; // Name of the file that contains site id and their locations in latlon WGS84
+var id = 'ID'; // Name of the column containing site id 
+
+var startYear=1980;
+var endYear=2000;//not included
+
+var GLDASv = '_GLDASv20'; // 1980-1999
+//var GLDASv = '_GLDASv21' // >=2000
+
+
+//--------------
+// Import
+//--------------
+// Plots
+
+var plots = ee.FeatureCollection("users/NXA807/"+siteFile); // change to GEE username
+Map.addLayer(plots);
+
+// SoilMoi0_10cm_inst	unit kg/m^2	min 1.99 max	47.59
+// SoilMoi10_40cm_inst unit kg/m^2 min 5.99	max 142.8
+// SoilMoi40_100cm_inst	unit kg/m^2	min 11.99	max 285.6	
+// SoilMoi100_200cm_inst  unit kg/m^2 min 20 max 476
+
+// Layer to import 
+var lyr_names = ['SoilMoi0_10cm_inst','SoilMoi10_40cm_inst','SoilMoi40_100cm_inst','SoilMoi100_200cm_inst'];
+
+for (var i = startYear; i<endYear ;i++) { 
+//for (var i = 1980; i<2000 ;i++) { // for years <2000
+
+var year = ee.String(ee.Number(i));
+var yearE = ee.String(ee.Number(i+1)); // excluded, this is to include 31 december of year i
+
+
+//var vis = {min: 1.99, max: 47.59, palette: ['blue', 'green', 'yellow', 'red']};
+//Map.addLayer(lyr, vis, '');
+
+// ESA soil moisture also 0.25 resolution
+//var lyr = ee.ImageCollection('NASA/GLDAS/V021/NOAH/G025/T3H').select(lyr_names).filter(ee.Filter.date(year.cat('-01-01'), yearE.cat('-01-01'))).sort('system:time_start'); // GLDAS v 2.1
+var lyr = ee.ImageCollection('NASA/GLDAS/V20/NOAH/G025/T3H').select(lyr_names).filter(ee.Filter.date(year.cat('-01-01'), yearE.cat('-01-01'))).sort('system:time_start'); // GLDAS v 2.0 for years <2000
+//print(lyr.limit(5))
+//Map.addLayer(lyr, {}, 'lyr');
+
+
+//--------------
+// Extract stats 
+//--------------
+
+// Average soil moisture
+var lyr = lyr.mean().reproject(lyr.first().projection());
+var bandNames = lyr.bandNames(); 
+print(bandNames);
+print(lyr);
+//Map.addLayer(lyr.select(1))
+
+// Points
+var addVar = function (f1) { 
+  var addVarBand = function (band, f2) {
+     band = ee.String(band)
+     f2 = ee.Feature(f2)
+    var varBand = ee.String(band)
+    var l = lyr.select(band);
+    var r = l.reduceRegion({
+            geometry: f1.geometry(),
+            reducer: ee.Reducer.first(),
+            //scale: res
+            }).get(band)
+                return f2.set(
+                  varBand, r
+                  )
+            } 
+  var f = ee.Feature(bandNames.iterate(addVarBand, f1))
+    return ee.Feature(null, f.toDictionary(
+      bandNames.add(id)))
+};
+
+var outPts = plots.map(addVar);
+print(outPts.first().toDictionary(), 'Data extracted for one example plot point');
+
+
+
+//----------------------
+// Export
+//----------------------
+
+var tabPts = ee.FeatureCollection(outPts.toList(outPts.size()));
+print(tabPts.limit(5));
+Export.table.toDrive({
+    collection: tabPts, 
+    folder: 'Shrub_sites_update_2021-12-31',
+    description: 'shrub_sites_NA_SoilMoisture_kgM2_Means_'+i+GLDASv,
+    fileFormat: 'CSV'
+});
+
+}
